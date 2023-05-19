48a7371d43cb7ea520e477bc8dddcd3615901f4b
高庆丰 <gao.qingfeng@gmail.com>
2023-05-19 23:37:55 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -598,7 +598,7 @@ impl Voltage {
         };
 
         //根据角度计算当前扇区
-        let sector = libm::floorf(angle_el / PI_3) as i32 + 1;
+        let sector = libm::floorf(angle_el / PI_3) as u32 + 1;
         let T1 = sector as f32 * PI_3 - angle_el;
 
         println!(
@@ -606,7 +606,7 @@ impl Voltage {
             angle_el.is_normal(),
             angle_el.is_nan()
         );
-        let T1 = libm::sinf(sector as f32 * PI_3 - angle_el);
+        let T1 = libm::sinf(T1);
         println!("T1={T1}");
         let T1 = SQRT_3 * T1;
         println!("T1={T1}");
